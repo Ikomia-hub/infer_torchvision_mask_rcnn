6c8d23251a496fcb9edb7d54efafa3f829859242
Ludo <ludovic.barusseau@gmail.com>
2021-09-16 09:43:13 +0200

    Add class name as category to box graphics

--- MaskRCNN_process.py
+++ MaskRCNN_process.py
@@ -173,7 +173,8 @@ class MaskRCNNProcess(dataprocess.C2dImageTask):
             box = boxes[i]
             w = box[2] - box[0]
             h = box[3] - box[1]
-            graphics_output.addRectangle(float(box[0]), float(box[1]), float(w), float(h), prop_rect)
+            graphics_box = graphics_output.addRectangle(float(box[0]), float(box[1]), float(w), float(h), prop_rect)
+            graphics_box.setCategory(self.class_names[labels[i]])
             # label
             label = self.class_names[labels[i]] + ": {:.3f}".format(scores[i])
             graphics_output.addText(label, float(box[0]), float(box[1]), prop_text)
